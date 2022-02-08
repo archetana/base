acffb3b13f844f0d0e678a9eeade9705904d9654
archetana <archetana@users.noreply.github.com>
2022-02-08 22:48:11 +0530

    Update install-pyrequirements.sh

--- install-pyrequirements.sh
+++ install-pyrequirements.sh
@@ -3,4 +3,5 @@ set -x #echo on
 
 python3 -m venv venv
 source /venv/bin/activate
+python3 -m pip install --no-cache-dir cython
 python3 -m pip install --no-cache-dir -r /app/python/requirements.txt
