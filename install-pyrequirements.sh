b5fd8720b07c845145bb97ab9337ad3aecaa5178
archetana <archetana@users.noreply.github.com>
2022-07-08 15:30:38 +0530

    Remove log4j delete

--- install-pyrequirements.sh
+++ install-pyrequirements.sh
@@ -5,4 +5,3 @@ python3 -m venv venv
 source /venv/bin/activate
 python3 -m pip install --no-cache-dir cython
 python3 -m pip install --no-cache-dir -r /app/python/requirements.txt
-rm -f /venv/lib/python3.9/site-packages/pyspark/jars/log4j-1.2.17.jar
